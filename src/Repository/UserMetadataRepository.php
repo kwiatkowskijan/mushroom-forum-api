fb1f2f4cac99bc45e3592bea6fb53b93fd13c1d1
kwiatkowskijan <kwiatkojan@gmail.com>
2025-06-25 20:24:45 +0200

    saved UNDO information for reset changes and discarded files

--- src/Repository/UserMetadataRepository.php
+++ src/Repository/UserMetadataRepository.php
@@ -16,28 +16,28 @@ class UserMetadataRepository extends ServiceEntityRepository
         parent::__construct($registry, UserMetadata::class);
     }
 
-//    /**
-//     * @return UserMetadata[] Returns an array of UserMetadata objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
+    //    /**
+    //     * @return UserMetadata[] Returns an array of UserMetadata objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
 
-//    public function findOneBySomeField($value): ?UserMetadata
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
+    //    public function findOneBySomeField($value): ?UserMetadata
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
