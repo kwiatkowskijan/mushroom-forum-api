91bc9b82b73017c46542651611c6a6f41f9f29c4
kwiatkowskijan <kwiatkojan@gmail.com>
2025-06-26 20:22:01 +0200

    Add user CLI command

---
+++ src/Command/AddUserCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'AddUser',
+    description: 'Add a short description for your command',
+)]
+class AddUserCommand extends Command
+{
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+        $this->em = $em;
+        $this->passwordHasher = $passwordHasher;
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->setDescription('Add a new user')
+            ->addArgument('email', InputArgument::REQUIRED, 'User email')
+            ->addArgument('password', InputArgument::REQUIRED, 'User password');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        // Sprawdź czy użytkownik już istnieje
+        $existing = $this->em->getRepository(User::class)->findOneBy(['email' => $email]);
+        if ($existing) {
+            $io->error('User with this email already exists.');
+            return Command::FAILURE;
+        }
+
+        $user = new User();
+        $user->setEmail($email);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
+        $user->setPassword($hashedPassword);
+        $user->setRoles(['ROLE_USER']);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('User created successfully!');
+        return Command::SUCCESS;
+    }
+}
