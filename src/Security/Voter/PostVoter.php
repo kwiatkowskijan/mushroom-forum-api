c7d22610007c1bbfd415efb6cd9953b5477bfa41
kwiatkowskijan <kwiatkojan@gmail.com>
2025-06-26 19:35:01 +0200

    Comment and post voter

---
+++ src/Security/Voter/PostVoter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Post;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+
+class PostVoter extends Voter
+{
+    public const EDIT = 'POST_EDIT';
+    public const DELETE = 'POST_DELETE';
+
+    protected function supports(string $attribute, $subject): bool
+    {
+        return in_array($attribute, [self::EDIT, self::DELETE])
+            && $subject instanceof Post;
+    }
+
+    protected function voteOnAttribute(string $attribute, $post, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+
+        if (!$user instanceof User) {
+            return false;
+        }
+
+        return $post->getAuthor() === $user;
+    }
+}
+
